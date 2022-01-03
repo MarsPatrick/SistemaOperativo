c3c85a317dfbadfe4585e592cdbe834877609ece
MarsPatrick <36931248+MarsPatrick@users.noreply.github.com>
2022-01-02 23:57:58 -0300

    a

--- Servidor.c
+++ Servidor.c
@@ -8,19 +8,34 @@
 #include <arpa/inet.h>
 
 #define PORT 4444
+int naleatorio();
+int nvalores(int x);
+int existe(int x[],int y, int z);
+int jugadores(int x);
 
 int main(){
 
 	int sockfd, ret;
-	 struct sockaddr_in serverAddr;
-
+	struct sockaddr_in serverAddr;
 	int newSocket;
 	struct sockaddr_in newAddr;
-
 	socklen_t addr_size;
-
+	
+	int fd1[2];
+	int fd2[2];
+	int tube1 = pipe(fd1);
+	int tube2 = pipe(fd2);
 	char buffer[1024];
 	pid_t childpid;
+
+	if(tube1 < 0) {
+        printf("Error al crear pipe 1\n");
+        exit(1);
+    }
+	if(tube2 < 0) {
+        printf("Error al crear pipe 2\n");
+        exit(1);
+    }
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if(sockfd < 0){
@@ -47,6 +62,108 @@ int main(){
 		printf("[-]Error in binding.\n");
 	}
 
+  
+	int x,y,z,cont,cont1,temp,tam,jug;
+
+    //Ingresar cantidad jugadores
+    jug = 4;
+    while (jug < 2 || jug > 4)
+    {
+        printf("Ingrese Numero Jugadores: ");
+        //scan numero jugadores
+        //jug = scan
+    }
+
+    //Iniciar la matriz
+    x = jugadores(jug);
+    y = jugadores(jug);
+    int matriz[x][y];
+
+    //Generar los numeros aleatorios
+    z = nvalores(x); 
+    int valores[z];
+    cont = 0;
+    while(z > 0){
+        temp = naleatorio(50,1);
+        if(cont > 0){
+            if(existe(valores,cont,temp) == 0){
+                valores[cont] = temp;
+                z--;
+                cont++;
+            }
+        }else{
+            valores[cont] = temp;
+            z--;
+            cont++;
+        }
+    }
+    
+    //Generar posiciones aleatorias para los numeros
+    z = nvalores(x);
+    int posicion[z];
+    cont = 0;
+    while(z > 0){
+        temp = naleatorio((x*y),1);
+        if(cont > 0){
+            if(existe(posicion,cont,temp) == 0){
+                posicion[cont] = temp;
+                z--;
+                cont++;
+            }
+        }else{
+            posicion[cont] = temp;
+            z--;
+            cont++;
+        }
+        
+    }
+
+    //Imprimir los numeros
+    for(int a = 0 ; a < nvalores(x) ; a++){
+        printf("%d ",valores[a]);
+    }
+    printf("\n");
+
+    //Imprimir las posiciones
+    for(int a = 0 ; a < nvalores(x) ; a++){
+        printf("%d ",posicion[a]);
+    }
+    printf("\n");
+
+    //Llenar la matriz con los numeros en sus respectivas posiciones
+    cont = 1;
+    cont1 = 0;
+    for(int a = 0 ; a < x ; a++){
+        for(int b = 0 ; b < y ; b++){
+            if(existe(posicion,nvalores(x),cont)==1){
+                matriz[a][b] = valores[cont1];
+                cont1++;
+                printf("%d %d\n",matriz[a][b],cont);
+            }else{
+                matriz[a][b] = 0;
+            }
+            cont++;
+        }
+    }
+
+    //Imprimir la matriz
+    for(int a = 0 ; a < x ; a++){
+        for(int b = 0 ; b < y ; b++){
+            if(matriz[a][b] < 10){
+                printf("[0%d]",matriz[a][b]);
+            }else{
+                printf("[%d]",matriz[a][b]);
+            }
+            
+        }
+        printf("\n");
+    }
+  
+  
+  
+  
+  
+  
     int x = 1;
 
 	while(1){
@@ -73,12 +190,35 @@ int main(){
 				}else{
 					//Imprime lo que el cliente mando
 					printf("Client: %s\n", buffer);
+					close(fd1[0]);
+            		close(fd2[1]);
+					if(read(fd2[0],buffer,sizeof(buffer)) > 0) {
+                	printf("El padre dijo: %s\n",buffer);
+            		}
+            		printf("El hijo manda: ");
+            		
+
+            		write(fd1[1],buffer,sizeof(buffer));
 
 					//Devuelve lo que se recibio al cliente
 					send(newSocket, buffer, strlen(buffer), 0);
 					bzero(buffer, sizeof(buffer));
 				}
 			}
+		}else{
+			while(1) {
+            close(fd1[1]);
+            close(fd2[0]);
+            printf("El padre manda: ");
+            
+
+            write(fd2[1],buffer,sizeof(buffer));
+
+            if(read(fd1[0],buffer,sizeof(buffer)) > 0) {
+                printf("El hijo escribe: %s\n",buffer);
+            }
+
+        }
 		}
 
 	}
@@ -87,4 +227,44 @@ int main(){
 
 
 	return 0;
+}
+
+int naleatorio(int y , int z){
+    int x;
+    srand (time(NULL));
+    x = rand()%y+z;
+    return x;
+}
+
+int nvalores(int x){
+    if(x = 8){
+        return 16;
+    }
+    if(x = 10){
+        return 25;
+    }
+    if(x = 12){
+        return 36;
+    }
+}
+
+int existe(int x[] , int y , int z){
+    for(int a = 0 ; a < y ; a++){
+        if(z == x[a]){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int jugadores(int x){
+    if(x == 2){
+        return 8;
+    }
+    if(x == 3){
+        return 10;
+    }
+    if(x == 4){
+        return 12;
+    }
 }
